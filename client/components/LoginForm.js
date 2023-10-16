14ebd7e0263c43c5ed1f539890d5668f9bc196fb
Nischhal Shrestha (Nokia) <nischhal.shrestha@nokia.com>
2023-10-16 21:39:28 +0300

    added register and login form

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,15 +1,84 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
-import { FormButton, FormInput, FormNavigationText } from "./FormComponents";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import {
+  ErrorMessage,
+  FormButton,
+  FormInput,
+  FormNavigationText,
+} from "./FormComponents";
 import Logo from "../views/Logo";
+import { useForm, Controller } from "react-hook-form";
 
 const LoginForm = (props) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data) => {
+    // Handle form submission
+    console.log("data", data);
+  };
   return (
     <SafeAreaView style={{ height: "100%" }}>
       <Logo />
-      <FormInput title="Username" />
-      <FormInput title="Password" />
-      <FormButton title="Login" navigation={props.navigation} />
+      <View>
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <FormInput
+              title="Username"
+              value={field.value}
+              onChange={field.onChange}
+              secret={false}
+            />
+          )}
+          name="username"
+          rules={{ required: "Name is required" }}
+        />
+        <ErrorMessage
+          error={errors?.username}
+          message={errors?.username?.message}
+        />
+
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <FormInput
+              placeholder="Enter your password"
+              secret={true}
+              value={field.value}
+              onChange={field.onChange}
+            />
+          )}
+          name="password"
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          }}
+        />
+        <ErrorMessage
+          error={errors?.password}
+          message={errors?.password?.message}
+        />
+
+        <FormButton title="Login" submit={handleSubmit(onSubmit)} />
+      </View>
       <FormNavigationText
         title="Don't have an account? Register Here!"
         formToggle={props.formToggle}
--- client/components/RegisterForm.js
+++ client/components/RegisterForm.js
@@ -1,22 +1,119 @@
 import React from "react";
 import { Dimensions, SafeAreaView, ScrollView, StyleSheet } from "react-native";
-import { FormButton, FormInput, FormNavigationText } from "./FormComponents";
+import {
+  ErrorMessage,
+  FormButton,
+  FormInput,
+  FormNavigationText,
+} from "./FormComponents";
 import Logo from "../views/Logo";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-
+import { useForm, Controller } from "react-hook-form";
 const RegisterForm = (props) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  const onSubmit = (data) => {
+    // Handle form submission
+    console.log("data", data);
+  };
   return (
     <SafeAreaView style={{ height: "100%" }}>
       <ScrollView>
         <Logo />
-        <FormInput title="Username" />
-        <FormInput title="E-mail" />
-        <FormInput title="Password" />
-        <FormInput title="Confirm Password" />
-        <FormButton title="Register" />
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <FormInput
+              title="Username"
+              value={field.value}
+              onChange={field.onChange}
+              secret={false}
+            />
+          )}
+          name="username"
+          rules={{ required: "Name is required" }}
+        />
+        <ErrorMessage
+          error={errors?.username}
+          message={errors?.username?.message}
+        />
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <FormInput
+              title="E-mail"
+              value={field.value}
+              onChange={field.onChange}
+              secret={false}
+            />
+          )}
+          name="email"
+          rules={{ required: "Email is required" }}
+        />
+        <ErrorMessage error={errors?.email} message={errors?.email?.message} />
+
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <FormInput
+              title="Password"
+              placeholder="Enter your password"
+              secret={true}
+              value={field.value}
+              onChange={field.onChange}
+            />
+          )}
+          name="password"
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          }}
+        />
+        <ErrorMessage
+          error={errors?.password}
+          message={errors?.password?.message}
+        />
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <FormInput
+              title="Confirm Password"
+              placeholder="Enter your password"
+              secret={true}
+              value={field.value}
+              onChange={field.onChange}
+            />
+          )}
+          name="confirmPassword"
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          }}
+        />
+        <ErrorMessage
+          error={errors?.confirmPassword}
+          message={errors?.confirmPassword?.message}
+        />
+        <FormButton title="Register" submit={handleSubmit(onSubmit)} />
         <FormNavigationText
           title="Already have an account? Login Here!"
           formToggle={props.formToggle}
